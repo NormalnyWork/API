4402d675183d66934501e37e48d26ba472a059d3
sightless <Twinkg06@mail.ru>
2025-05-10 01:30:07 +0500

    refactor: comment out

--- src/service/fcm_service.py
+++ src/service/fcm_service.py
@@ -1,20 +1,20 @@
-import firebase_admin
-from firebase_admin import credentials, messaging
-
-from config import get_settings
-
-settings = get_settings()
-
-cred = credentials.Certificate(settings.firebase_credentials_path)
-firebase_admin.initialize_app(cred)
-
-def send_fcm_notification(token: str, title: str, body: str):
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title=title,
-            body=body
-        ),
-        token=token,
-    )
-    response = messaging.send(message)
-    return response
+# import firebase_admin
+# from firebase_admin import credentials, messaging
+#
+# from config import get_settings
+#
+# settings = get_settings()
+#
+# cred = credentials.Certificate(settings.firebase_credentials_path)
+# firebase_admin.initialize_app(cred)
+#
+# def send_fcm_notification(token: str, title: str, body: str):
+#     message = messaging.Message(
+#         notification=messaging.Notification(
+#             title=title,
+#             body=body
+#         ),
+#         token=token,
+#     )
+#     response = messaging.send(message)
+#     return response
